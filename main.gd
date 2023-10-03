286b5186fb081b8cd40f498f5876075bf509577d
ED0LEO <ED0LEO@proton.me>
2023-10-03 17:48:56 +0200

    Retry option is coded

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$UserInterface/Retry.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -35,3 +35,10 @@ func _on_mob_timer_timeout():
 
 func _on_player_hit():
 	$MobTimer.stop()
+	$UserInterface/Retry.show()
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept") and $UserInterface/Retry.visible:
+		# This restarts the current scene.
+		get_tree().reload_current_scene()
